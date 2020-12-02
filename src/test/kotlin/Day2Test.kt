d1b34391ecef7b4f4a66e4d1c9b9d9d307a56bd9
yangzii0920 <11530441+yangzii0920@users.noreply.github.com>
2020-12-02 01:48:14 -0500

    day2: part one O(nm); part two O(n)

---
+++ src/test/kotlin/Day2Test.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test
+
+internal class Day2Test {
+
+    private val day2 = Day2()
+
+    @Test
+    fun partOne() {
+        println(day2.partOne())
+    }
+
+    @Test
+    fun partTwo() {
+        println(day2.partTwo())
+    }
+}
