e1d3243cabcbe4ba124228bba53ce0e5fa2a3969
yangzii0920 <11530441+yangzii0920@users.noreply.github.com>
2020-12-21 23:24:10 -0500

    day 13 part 2 chinese remainder theorem

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -16,14 +16,27 @@ class Day13 {
 
     fun partTwo(): Long {
         val input = File("src/main/resources/Day13.input").readLines()
-        val times = input[1].split(",").mapIndexed { index, id -> if (id == "x") -1 to index else id.toInt() to index }.filter { it.first != -1 }.sortedByDescending { it.first }
-        var timestamp: Long = 0
-        val step = times[0].first
-        while (timestamp <= Long.MAX_VALUE) {
-            if (times.all { (timestamp + (it.second - times[0].second)).rem(it.first).toInt() == 0 })
-                return timestamp - times[0].second
-            timestamp += step
-        }
-        return -1
+        val times = input[1].split(",")
+            .mapIndexed { index, id -> if (id == "x") -1L to index.toLong() else id.toLong() to index.toLong() }
+            .filter { it.first != -1L }
+        val prod = times.map { it.first }.reduce { acc, i -> acc * i }
+
+        val found = chineseRemainderTheorem(prod, times.map { it.first }, times.map { if (it.second == 0L) 0 else it.first - (it.second.rem(it.first)) } )
+        return found - found / prod * prod
     }
+
+     private fun chineseRemainderTheorem(prod: Long, divisors: List<Long>, remainders: List<Long>): Long {
+        return divisors.mapIndexed { i, divisor ->
+            val n = prod / divisor
+            var d = 1L
+            while (d <= divisor) {
+                if ((n * d).rem(divisor) == remainders[i]) {
+                     break
+                }
+                d += 1
+            }
+            n * d
+        }.sum()
+    }
+
 }
